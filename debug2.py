52e0903c20d5111c7c65076dc34dac909feb3d93
Wahaj Al Obid <wahajalobid@hotmail.com>
2024-10-25 11:07:05 -0700

    Add files via upload

--- debug2.py
+++ debug2.py
@@ -1,15 +1,27 @@
-def lone_sum(a, b, c):
-if a >= b:
-return c
-elif a == c:
-return b
-elif b == c:
-return a
-elif a == b and a == c and b == c:
-return 0
-else:
-return a+b+c
-print("lone_sum of 10, 10, 10 should be 0: " + str(lone_sum(10, 10, 10)))
-print("lone_sum of 1, 2, 3 should be 6: " + str(lone_sum(1, 2, 3)))
-print("lone_sum of 1, 2, 1 should be 2: " + str(lone_sum(1, 2, 1)))
-print("lone_sum of 4, 5, 6 should be 15: " + str(lone_sum(4, 5, 6)))
+def score_to_letter_grade(grade):
+    if grade > 90:
+        return "A"
+    elif grade >= 87:
+        return "B+"
+    elif grade == 80:
+        return "B"
+    elif grade >= 77:
+        return "C+"
+    elif grade <= 70:
+        return "C"
+    elif grade >= 67:
+        return "D+"
+    elif grade >= 60:
+        return "D"
+    else:
+        return "F"
+
+
+print("Grade of 90 should be A: " + score_to_letter_grade(90))
+print("Grade of 87 should be B+: " + score_to_letter_grade(87))
+print("Grade of 81 should be B: " + score_to_letter_grade(81))
+print("Grade of 77 should be C+: " + score_to_letter_grade(77))
+print("Grade of 70 should be C: " + score_to_letter_grade(70))
+print("Grade of 67 should be D+: " + score_to_letter_grade(67))
+print("Grade of 60 should be D: " + score_to_letter_grade(60))
+print("Grade of 59 should be F: " + score_to_letter_grade(59))
